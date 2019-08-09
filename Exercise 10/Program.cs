586fcfd3b7351a125f2c6fa0e542afa2d44679b1
mattbak3r <47476895+mattbak3r@users.noreply.github.com>
2019-08-09 07:36:24 -0700

    Finished

--- Exercise 10/Program.cs
+++ Exercise 10/Program.cs
@@ -17,19 +17,32 @@ namespace Exercise_10
         static void Main(string[] args)
         {
             String input = "Don't be late, teatime will not wait!";
+            String inputTwo = "Don b la(te), teatim will no wai(t)!";
             Console.WriteLine(input);
             Console.WriteLine("There are " + countTE(input) + " words that end in 't' or 'e'.");
+            Console.WriteLine("");
+            Console.WriteLine(inputTwo);
+            Console.WriteLine("There are " + countTE(inputTwo) + " words that end in 't' or 'e'.");
         }
+        //Splits string to array of words, splits words to array of characters, checks last array index for 'e' or 't', count++ if 'e' or 't' is last index.
         public static int countTE(String str)
         {
             int count = 0;
-            char[] delimiterChars = { ' ', ',', '-', '!', ':' };
+            char[] delimiterChars = { ' ', ',', '-', '!', ':', ')', '(' };
             String[] split = str.Split(delimiterChars);
+            
             for (int i = 0; i < split.Length; i++)
             {
                 String word = split[i].ToLower();
                 int length = word.Length - 1;
-                if (length > 0)
+                if (length == 0)
+                {
+                    if (word == "e" || word == "t")
+                    {
+                        count++;
+                    }
+                }
+                else if (length > 0)
                 {
                     char[] lastLetter = word.ToCharArray();
                     int wordLentgh = lastLetter.Length - 1;
